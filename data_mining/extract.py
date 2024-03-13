5465f19ea37f61dcafa08f9bf0abeefdaf83af19
Kaushil <kaushil.nagrale@gmail.com>
2024-03-13 23:44:11 +0530

    Initial commit

---
+++ data_mining/extract.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from PIL import Image
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+import time
+import pandas as pd
+import os
+from selenium.webdriver.chrome.options import Options
+import zipfile
+
+
